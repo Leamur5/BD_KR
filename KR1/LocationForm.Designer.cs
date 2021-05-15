3ed6f5e0f284c3bf79aaf20010d21024b46963b0
Leamur5 <leamur5@mail.ru>
2021-05-15 15:01:43 +0300

    location form done

--- KR1/LocationForm.Designer.cs
+++ KR1/LocationForm.Designer.cs
@@ -30,11 +30,305 @@ namespace KR1
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.formLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cityTextBox = new System.Windows.Forms.TextBox();
+            this.streetTextBox = new System.Windows.Forms.TextBox();
+            this.houseTextBox = new System.Windows.Forms.TextBox();
+            this.floorTextBox = new System.Windows.Forms.TextBox();
+            this.cabinetTextBox = new System.Windows.Forms.TextBox();
+            this.locationDataGridView = new System.Windows.Forms.DataGridView();
+            this.bD_KRDataSet = new KR1.BD_KRDataSet();
+            this.locationBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.locationTableAdapter = new KR1.BD_KRDataSetTableAdapters.LocationTableAdapter();
+            this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cityDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.streetDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.houseDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.floorDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cabinetDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.controlsPanel = new System.Windows.Forms.Panel();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.locationDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bD_KRDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.locationBindingSource)).BeginInit();
+            this.controlsPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // formLabel
+            // 
+            this.formLabel.AutoSize = true;
+            this.formLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.formLabel.Location = new System.Drawing.Point(55, 9);
+            this.formLabel.Name = "formLabel";
+            this.formLabel.Size = new System.Drawing.Size(231, 31);
+            this.formLabel.TabIndex = 3;
+            this.formLabel.Text = "Местоположение";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 116;
+            this.label2.Text = "Город";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 117;
+            this.label1.Text = "Улица";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(68, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 118;
+            this.label3.Text = "Дом";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(370, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 119;
+            this.label4.Text = "Этаж";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(370, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 120;
+            this.label5.Text = "Кабинет";
+            // 
+            // cityTextBox
+            // 
+            this.cityTextBox.Location = new System.Drawing.Point(120, 76);
+            this.cityTextBox.Name = "cityTextBox";
+            this.cityTextBox.Size = new System.Drawing.Size(152, 20);
+            this.cityTextBox.TabIndex = 121;
+            // 
+            // streetTextBox
+            // 
+            this.streetTextBox.Location = new System.Drawing.Point(120, 116);
+            this.streetTextBox.Name = "streetTextBox";
+            this.streetTextBox.Size = new System.Drawing.Size(152, 20);
+            this.streetTextBox.TabIndex = 122;
+            // 
+            // houseTextBox
+            // 
+            this.houseTextBox.Location = new System.Drawing.Point(120, 158);
+            this.houseTextBox.Name = "houseTextBox";
+            this.houseTextBox.Size = new System.Drawing.Size(152, 20);
+            this.houseTextBox.TabIndex = 123;
+            // 
+            // floorTextBox
+            // 
+            this.floorTextBox.Location = new System.Drawing.Point(430, 76);
+            this.floorTextBox.Name = "floorTextBox";
+            this.floorTextBox.Size = new System.Drawing.Size(152, 20);
+            this.floorTextBox.TabIndex = 124;
+            // 
+            // cabinetTextBox
+            // 
+            this.cabinetTextBox.Location = new System.Drawing.Point(430, 116);
+            this.cabinetTextBox.Name = "cabinetTextBox";
+            this.cabinetTextBox.Size = new System.Drawing.Size(152, 20);
+            this.cabinetTextBox.TabIndex = 125;
+            // 
+            // locationDataGridView
+            // 
+            this.locationDataGridView.AutoGenerateColumns = false;
+            this.locationDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.locationDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.iDDataGridViewTextBoxColumn,
+            this.cityDataGridViewTextBoxColumn,
+            this.streetDataGridViewTextBoxColumn,
+            this.houseDataGridViewTextBoxColumn,
+            this.floorDataGridViewTextBoxColumn,
+            this.cabinetDataGridViewTextBoxColumn});
+            this.locationDataGridView.DataSource = this.locationBindingSource;
+            this.locationDataGridView.Location = new System.Drawing.Point(71, 213);
+            this.locationDataGridView.Name = "locationDataGridView";
+            this.locationDataGridView.Size = new System.Drawing.Size(645, 150);
+            this.locationDataGridView.TabIndex = 126;
+            this.locationDataGridView.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.locationDataGridView_RowHeaderMouseClick);
+            // 
+            // bD_KRDataSet
+            // 
+            this.bD_KRDataSet.DataSetName = "BD_KRDataSet";
+            this.bD_KRDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // locationBindingSource
+            // 
+            this.locationBindingSource.DataMember = "Location";
+            this.locationBindingSource.DataSource = this.bD_KRDataSet;
+            // 
+            // locationTableAdapter
+            // 
+            this.locationTableAdapter.ClearBeforeFill = true;
+            // 
+            // iDDataGridViewTextBoxColumn
+            // 
+            this.iDDataGridViewTextBoxColumn.DataPropertyName = "ID";
+            this.iDDataGridViewTextBoxColumn.HeaderText = "ID";
+            this.iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn";
+            // 
+            // cityDataGridViewTextBoxColumn
+            // 
+            this.cityDataGridViewTextBoxColumn.DataPropertyName = "City";
+            this.cityDataGridViewTextBoxColumn.HeaderText = "City";
+            this.cityDataGridViewTextBoxColumn.Name = "cityDataGridViewTextBoxColumn";
+            // 
+            // streetDataGridViewTextBoxColumn
+            // 
+            this.streetDataGridViewTextBoxColumn.DataPropertyName = "Street";
+            this.streetDataGridViewTextBoxColumn.HeaderText = "Street";
+            this.streetDataGridViewTextBoxColumn.Name = "streetDataGridViewTextBoxColumn";
+            // 
+            // houseDataGridViewTextBoxColumn
+            // 
+            this.houseDataGridViewTextBoxColumn.DataPropertyName = "House";
+            this.houseDataGridViewTextBoxColumn.HeaderText = "House";
+            this.houseDataGridViewTextBoxColumn.Name = "houseDataGridViewTextBoxColumn";
+            // 
+            // floorDataGridViewTextBoxColumn
+            // 
+            this.floorDataGridViewTextBoxColumn.DataPropertyName = "Floor";
+            this.floorDataGridViewTextBoxColumn.HeaderText = "Floor";
+            this.floorDataGridViewTextBoxColumn.Name = "floorDataGridViewTextBoxColumn";
+            // 
+            // cabinetDataGridViewTextBoxColumn
+            // 
+            this.cabinetDataGridViewTextBoxColumn.DataPropertyName = "Cabinet";
+            this.cabinetDataGridViewTextBoxColumn.HeaderText = "Cabinet";
+            this.cabinetDataGridViewTextBoxColumn.Name = "cabinetDataGridViewTextBoxColumn";
+            // 
+            // controlsPanel
+            // 
+            this.controlsPanel.Controls.Add(this.deleteButton);
+            this.controlsPanel.Controls.Add(this.addButton);
+            this.controlsPanel.Controls.Add(this.closeButton);
+            this.controlsPanel.Controls.Add(this.saveButton);
+            this.controlsPanel.Location = new System.Drawing.Point(165, 378);
+            this.controlsPanel.Name = "controlsPanel";
+            this.controlsPanel.Size = new System.Drawing.Size(471, 107);
+            this.controlsPanel.TabIndex = 127;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(347, 6);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(81, 41);
+            this.deleteButton.TabIndex = 57;
+            this.deleteButton.Text = "Удалить запись";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(18, 6);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(98, 40);
+            this.addButton.TabIndex = 54;
+            this.addButton.Text = "Добавить запись";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(186, 54);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(81, 42);
+            this.closeButton.TabIndex = 55;
+            this.closeButton.Text = "Закрыть форму";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(186, 6);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(84, 40);
+            this.saveButton.TabIndex = 53;
+            this.saveButton.Text = "Сохранить Запись";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // LocationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(799, 487);
+            this.Controls.Add(this.controlsPanel);
+            this.Controls.Add(this.locationDataGridView);
+            this.Controls.Add(this.cabinetTextBox);
+            this.Controls.Add(this.floorTextBox);
+            this.Controls.Add(this.houseTextBox);
+            this.Controls.Add(this.streetTextBox);
+            this.Controls.Add(this.cityTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.formLabel);
+            this.Name = "LocationForm";
             this.Text = "LocationForm";
+            this.Load += new System.EventHandler(this.LocationForm_Load);
+            this.Click += new System.EventHandler(this.LocationForm_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.locationDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bD_KRDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.locationBindingSource)).EndInit();
+            this.controlsPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label formLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox cityTextBox;
+        private System.Windows.Forms.TextBox streetTextBox;
+        private System.Windows.Forms.TextBox houseTextBox;
+        private System.Windows.Forms.TextBox floorTextBox;
+        private System.Windows.Forms.TextBox cabinetTextBox;
+        private System.Windows.Forms.DataGridView locationDataGridView;
+        private BD_KRDataSet bD_KRDataSet;
+        private System.Windows.Forms.BindingSource locationBindingSource;
+        private BD_KRDataSetTableAdapters.LocationTableAdapter locationTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cityDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn streetDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn houseDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn floorDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cabinetDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Panel controlsPanel;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Button saveButton;
     }
 }
--- KR1/LocationForm.cs
+++ KR1/LocationForm.cs
@@ -10,11 +10,147 @@ using System.Windows.Forms;
 
 namespace KR1
 {
-    public partial class LocationForm : Form
+    public partial class LocationForm : Form, ICRUDForm
     {
+        private static class LocationModel
+        {
+            public static int id { get; set; }
+            public static string city { get; set; }
+            public static string street { get; set; }
+            public static int house { get; set; }
+            public static int floor { get; set; }
+            public static int cabinet { get; set; }
+            public static void reset()
+            {
+                id = -1;
+                city = "";
+                street = "";
+                house = -1;
+                floor = -1;
+                cabinet = -1;
+            }
+        }
         public LocationForm()
         {
             InitializeComponent();
+            clearInputs();
+            LocationModel.reset();
+        }
+        public void updateView()
+        {
+            locationTableAdapter.Fill(bD_KRDataSet.Location);
+            locationBindingSource.ResetBindings(false);
+            locationDataGridView.Update();
+        }
+        public void insertRow()
+        {
+            locationTableAdapter.Insert(cityTextBox.Text, streetTextBox.Text, Convert.ToInt32(houseTextBox.Text),
+                Convert.ToInt32(floorTextBox.Text), Convert.ToInt32(cabinetTextBox.Text));
+        }
+        public void deleteRow()
+        {
+            locationTableAdapter.Delete(LocationModel.id, LocationModel.city,
+                LocationModel.street, LocationModel.house, LocationModel.floor, LocationModel.cabinet);
+        }
+        public void updateRow()
+        {
+            locationTableAdapter.Update(bD_KRDataSet);
+        }
+        public void updateInputs()
+        {
+            cityTextBox.Text = LocationModel.city;
+            streetTextBox.Text = LocationModel.street;
+            houseTextBox.Text = Convert.ToString(LocationModel.house);
+            floorTextBox.Text = Convert.ToString(LocationModel.floor);
+            cabinetTextBox.Text = Convert.ToString(LocationModel.cabinet);
+        }
+        public void clearInputs()
+        {
+            cityTextBox.Text = "";
+            streetTextBox.Text = "";
+            houseTextBox.Text = "";
+            floorTextBox.Text = "";
+            cabinetTextBox.Text = "";
+        }
+        public bool isIncorrectModel()
+        {
+            return LocationModel.id < 0 || LocationModel.city == "" ||
+                LocationModel.street == "" || LocationModel.house < 0 ||
+                LocationModel.floor < 0 || LocationModel.cabinet < 0;
+        }
+        public bool isIncorrectInputs()
+        {
+            return cityTextBox.Text == "" || streetTextBox.Text == ""
+                || houseTextBox.Text == "" || floorTextBox.Text == "" || cabinetTextBox.Text == "";
+        }
+        private void locationDataGridView_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            LocationModel.id = Convert.ToInt32(locationDataGridView.Rows[e.RowIndex].Cells[0].Value.ToString());
+            LocationModel.city = locationDataGridView.Rows[e.RowIndex].Cells[1].Value.ToString();
+            LocationModel.street = locationDataGridView.Rows[e.RowIndex].Cells[2].Value.ToString();
+            LocationModel.house = Convert.ToInt32(locationDataGridView.Rows[e.RowIndex].Cells[3].Value.ToString());
+            LocationModel.floor = Convert.ToInt32(locationDataGridView.Rows[e.RowIndex].Cells[4].Value.ToString());
+            LocationModel.cabinet = Convert.ToInt32(locationDataGridView.Rows[e.RowIndex].Cells[5].Value.ToString());
+
+            updateInputs();
+        }
+        private void LocationForm_Click(object sender, EventArgs e)
+        {
+            clearInputs();
+            LocationModel.reset();
+        }
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            if (isIncorrectInputs())
+            {
+                MessageBox.Show("Некоректные данные");
+            }
+            else
+            {
+                insertRow();
+                updateView();
+            }
+        }
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (isIncorrectModel() || isIncorrectInputs())
+            {
+                MessageBox.Show("Выберите строку и введите новые значения");
+            }
+            else
+            {
+                updateRow();
+
+                updateView();
+                LocationModel.reset();
+                clearInputs();
+            }
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            if (isIncorrectModel() || isIncorrectInputs())
+            {
+                MessageBox.Show("Выберите строку");
+            }
+            else
+            {
+                deleteRow();
+                updateView();
+                LocationModel.reset();
+                clearInputs();
+            }
+        }
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void LocationForm_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "bD_KRDataSet.Location". При необходимости она может быть перемещена или удалена.
+            this.locationTableAdapter.Fill(this.bD_KRDataSet.Location);
+
         }
     }
 }
