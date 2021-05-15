22e7ba251d6313dd7512f20d702a618749ba886c
Leamur5 <leamur5@mail.ru>
2021-05-15 18:01:27 +0300

    vse formi done

--- KR1/ProvidersForm.Designer.cs
+++ KR1/ProvidersForm.Designer.cs
@@ -30,11 +30,275 @@ namespace KR1
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.telephoneTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.adressComboBox = new System.Windows.Forms.ComboBox();
+            this.providersDataGridView = new System.Windows.Forms.DataGridView();
+            this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.providerNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.telephoneDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.adressDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.providerBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.bD_KRDataSet = new KR1.BD_KRDataSet();
+            this.providerTableAdapter = new KR1.BD_KRDataSetTableAdapters.ProviderTableAdapter();
+            this.purchasesDataGridView = new System.Windows.Forms.DataGridView();
+            this.controlsPanel = new System.Windows.Forms.Panel();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.locationBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.locationTableAdapter = new KR1.BD_KRDataSetTableAdapters.LocationTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.providersDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.providerBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bD_KRDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.purchasesDataGridView)).BeginInit();
+            this.controlsPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.locationBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(51, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(234, 42);
+            this.label1.TabIndex = 38;
+            this.label1.Text = "Поставщики";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 40;
+            this.label3.Text = "Наименование";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 41;
+            this.label2.Text = "Телефон";
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Location = new System.Drawing.Point(133, 90);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(152, 20);
+            this.NameTextBox.TabIndex = 43;
+            // 
+            // telephoneTextBox
+            // 
+            this.telephoneTextBox.Location = new System.Drawing.Point(133, 121);
+            this.telephoneTextBox.Name = "telephoneTextBox";
+            this.telephoneTextBox.Size = new System.Drawing.Size(152, 20);
+            this.telephoneTextBox.TabIndex = 44;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 45;
+            this.label4.Text = "Адрес";
+            // 
+            // adressComboBox
+            // 
+            this.adressComboBox.DataSource = this.locationBindingSource;
+            this.adressComboBox.DisplayMember = "ID";
+            this.adressComboBox.FormattingEnabled = true;
+            this.adressComboBox.Location = new System.Drawing.Point(133, 154);
+            this.adressComboBox.Name = "adressComboBox";
+            this.adressComboBox.Size = new System.Drawing.Size(152, 21);
+            this.adressComboBox.TabIndex = 107;
+            this.adressComboBox.ValueMember = "UnitId";
+            // 
+            // providersDataGridView
+            // 
+            this.providersDataGridView.AutoGenerateColumns = false;
+            this.providersDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.providersDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.iDDataGridViewTextBoxColumn,
+            this.providerNameDataGridViewTextBoxColumn,
+            this.telephoneDataGridViewTextBoxColumn,
+            this.adressDataGridViewTextBoxColumn});
+            this.providersDataGridView.DataSource = this.providerBindingSource;
+            this.providersDataGridView.Location = new System.Drawing.Point(335, 69);
+            this.providersDataGridView.Name = "providersDataGridView";
+            this.providersDataGridView.Size = new System.Drawing.Size(453, 134);
+            this.providersDataGridView.TabIndex = 108;
+            this.providersDataGridView.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.ProvidersDataGridView_RowHeaderMouseClick);
+            // 
+            // iDDataGridViewTextBoxColumn
+            // 
+            this.iDDataGridViewTextBoxColumn.DataPropertyName = "ID";
+            this.iDDataGridViewTextBoxColumn.HeaderText = "ID";
+            this.iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn";
+            // 
+            // providerNameDataGridViewTextBoxColumn
+            // 
+            this.providerNameDataGridViewTextBoxColumn.DataPropertyName = "ProviderName";
+            this.providerNameDataGridViewTextBoxColumn.HeaderText = "ProviderName";
+            this.providerNameDataGridViewTextBoxColumn.Name = "providerNameDataGridViewTextBoxColumn";
+            // 
+            // telephoneDataGridViewTextBoxColumn
+            // 
+            this.telephoneDataGridViewTextBoxColumn.DataPropertyName = "Telephone";
+            this.telephoneDataGridViewTextBoxColumn.HeaderText = "Telephone";
+            this.telephoneDataGridViewTextBoxColumn.Name = "telephoneDataGridViewTextBoxColumn";
+            // 
+            // adressDataGridViewTextBoxColumn
+            // 
+            this.adressDataGridViewTextBoxColumn.DataPropertyName = "Adress";
+            this.adressDataGridViewTextBoxColumn.HeaderText = "Adress";
+            this.adressDataGridViewTextBoxColumn.Name = "adressDataGridViewTextBoxColumn";
+            // 
+            // providerBindingSource
+            // 
+            this.providerBindingSource.DataMember = "Provider";
+            this.providerBindingSource.DataSource = this.bD_KRDataSet;
+            // 
+            // bD_KRDataSet
+            // 
+            this.bD_KRDataSet.DataSetName = "BD_KRDataSet";
+            this.bD_KRDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // providerTableAdapter
+            // 
+            this.providerTableAdapter.ClearBeforeFill = true;
+            // 
+            // purchasesDataGridView
+            // 
+            this.purchasesDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.purchasesDataGridView.Location = new System.Drawing.Point(164, 226);
+            this.purchasesDataGridView.Name = "purchasesDataGridView";
+            this.purchasesDataGridView.Size = new System.Drawing.Size(482, 150);
+            this.purchasesDataGridView.TabIndex = 109;
+            // 
+            // controlsPanel
+            // 
+            this.controlsPanel.Controls.Add(this.deleteButton);
+            this.controlsPanel.Controls.Add(this.addButton);
+            this.controlsPanel.Controls.Add(this.closeButton);
+            this.controlsPanel.Controls.Add(this.saveButton);
+            this.controlsPanel.Location = new System.Drawing.Point(164, 400);
+            this.controlsPanel.Name = "controlsPanel";
+            this.controlsPanel.Size = new System.Drawing.Size(471, 117);
+            this.controlsPanel.TabIndex = 110;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(347, 6);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(81, 41);
+            this.deleteButton.TabIndex = 57;
+            this.deleteButton.Text = "Удалить запись";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(18, 6);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(98, 40);
+            this.addButton.TabIndex = 54;
+            this.addButton.Text = "Добавить запись";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(186, 54);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(81, 49);
+            this.closeButton.TabIndex = 55;
+            this.closeButton.Text = "Закрыть форму";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(186, 6);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(84, 40);
+            this.saveButton.TabIndex = 53;
+            this.saveButton.Text = "Сохранить Запись";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // locationBindingSource
+            // 
+            this.locationBindingSource.DataMember = "Location";
+            this.locationBindingSource.DataSource = this.bD_KRDataSet;
+            // 
+            // locationTableAdapter
+            // 
+            this.locationTableAdapter.ClearBeforeFill = true;
+            // 
+            // ProvidersForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 539);
+            this.Controls.Add(this.controlsPanel);
+            this.Controls.Add(this.purchasesDataGridView);
+            this.Controls.Add(this.providersDataGridView);
+            this.Controls.Add(this.adressComboBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.telephoneTextBox);
+            this.Controls.Add(this.NameTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "ProvidersForm";
             this.Text = "ProvidersForm";
+            this.Load += new System.EventHandler(this.ProvidersForm_Load);
+            this.Click += new System.EventHandler(this.ProvidersForm_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.providersDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.providerBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bD_KRDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.purchasesDataGridView)).EndInit();
+            this.controlsPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.locationBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.TextBox telephoneTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox adressComboBox;
+        private System.Windows.Forms.DataGridView providersDataGridView;
+        private BD_KRDataSet bD_KRDataSet;
+        private System.Windows.Forms.BindingSource providerBindingSource;
+        private BD_KRDataSetTableAdapters.ProviderTableAdapter providerTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn providerNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn telephoneDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn adressDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridView purchasesDataGridView;
+        private System.Windows.Forms.Panel controlsPanel;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.BindingSource locationBindingSource;
+        private BD_KRDataSetTableAdapters.LocationTableAdapter locationTableAdapter;
     }
 }
--- KR1/ProvidersForm.cs
+++ KR1/ProvidersForm.cs
@@ -10,11 +10,154 @@ using System.Windows.Forms;
 
 namespace KR1
 {
-    public partial class ProvidersForm : Form
+    public partial class ProvidersForm : Form, ICRUDForm
     {
+        private static class ProvidersModel
+        {
+            public static int id { get; set; }
+            public static string name { get; set; }
+            public static string telephone { get; set; }
+            public static int adress { get; set; }
+            public static void reset()
+            {
+                id = -1;
+                name = "";
+                telephone = "";
+                adress = -1;
+                
+            }
+        }
         public ProvidersForm()
         {
             InitializeComponent();
+            adressComboBox.Update();
+
+            ProvidersModel.reset();
+            clearInputs();
+        }
+        public void updateView()
+        {
+            providerTableAdapter.Fill(bD_KRDataSet.Provider);
+            providerBindingSource.ResetBindings(false);
+            providersDataGridView.Update();
+        }
+        public void insertRow()
+        {
+            providerTableAdapter.Insert(NameTextBox.Text, telephoneTextBox.Text,
+                locationTableAdapter.GetData().First(u => u.ID == Convert.ToInt32(adressComboBox.Text)).ID);
+        }
+        public void deleteRow()
+        {
+            providerTableAdapter.Delete(ProvidersModel.id, ProvidersModel.name,
+                ProvidersModel.telephone, ProvidersModel.adress);
+        }
+        public void updateRow()
+        {
+            providerTableAdapter.Update(bD_KRDataSet);
+        }
+        public void updateInputs()
+        {
+            NameTextBox.Text = ProvidersModel.name;
+            telephoneTextBox.Text = ProvidersModel.telephone;
+            adressComboBox.SelectedIndex = adressComboBox.FindString(Convert.ToString(locationTableAdapter.GetData().FirstOrDefault(u => u.ID == ProvidersModel.adress).ID));
+
+            string query = "SELECT Purchases.ID,Purchases.Provider, Purchases.DeliveryDate,Purchases.GuaranteePeriod,Purchases.PurchaseAmount " +
+                "FROM(Provider INNER JOIN Purchases ON Provider.ID = Purchases.Provider) " +
+                "WHERE(Provider.ID = " + ProvidersModel.id.ToString() + ")";
+            purchasesDataGridView.DataSource = DGVDataSourceChanger.getNewDGVDataSource(query);
+            
+
+        }
+        public void clearInputs()
+        {
+            NameTextBox.Text = "";
+            telephoneTextBox.Text = "";
+            adressComboBox.SelectedIndex = -1;
+            
+
+            for (int i = 0; i < providersDataGridView.Rows.Count; i++)
+            {
+                providersDataGridView.Rows[i].Selected = false;
+            }
+            providersDataGridView.Update();
+        }
+        public bool isIncorrectModel()
+        {
+            return ProvidersModel.id < 0 || ProvidersModel.name.Length == 0 ||
+                ProvidersModel.telephone.Length == 0 || ProvidersModel.adress < 0;
+        }
+        public bool isIncorrectInputs()
+        {
+            return NameTextBox.Text.Trim().Length == 0 || telephoneTextBox.Text.Trim().Length == 0 ||
+                adressComboBox.SelectedIndex < 0;
+        }
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            if (isIncorrectInputs())
+            {
+                MessageBox.Show("Некоректные данные");
+            }
+            else
+            {
+                insertRow();
+                updateView();
+            }
+        }
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (isIncorrectModel() || isIncorrectInputs())
+            {
+                MessageBox.Show("Выберите строку и введите новые значения");
+            }
+            else
+            {
+                updateRow();
+                updateView();
+                ProvidersModel.reset();
+                clearInputs();
+            }
+        }
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            if (isIncorrectModel() || isIncorrectInputs())
+            {
+                MessageBox.Show("Выберите строку");
+            }
+            else
+            {
+                deleteRow();
+                updateView();
+                ProvidersModel.reset();
+                clearInputs();
+            }
+        }
+        private void ProvidersDataGridView_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            ProvidersModel.id = Convert.ToInt32(providersDataGridView.Rows[e.RowIndex].Cells[0].Value.ToString());
+            ProvidersModel.name = providersDataGridView.Rows[e.RowIndex].Cells[1].Value.ToString();
+            ProvidersModel.telephone = providersDataGridView.Rows[e.RowIndex].Cells[2].Value.ToString();
+            ProvidersModel.adress = Convert.ToInt32(providersDataGridView.Rows[e.RowIndex].Cells[3].Value.ToString());
+
+            updateInputs();
+        }
+        private void ProvidersForm_Click(object sender, EventArgs e)
+        {
+            ProvidersModel.reset();
+            clearInputs();
+        }
+
+
+        private void ProvidersForm_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "bD_KRDataSet.Location". При необходимости она может быть перемещена или удалена.
+            this.locationTableAdapter.Fill(this.bD_KRDataSet.Location);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "bD_KRDataSet.Provider". При необходимости она может быть перемещена или удалена.
+            this.providerTableAdapter.Fill(this.bD_KRDataSet.Provider);
+
         }
     }
 }
